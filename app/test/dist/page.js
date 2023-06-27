d46a53e70caf04971999e68fde72c7222ff47f72
shiyu <84095503+jjblau2021@users.noreply.github.com>
2023-06-27 14:54:03 +0800

    refactor: :recycle: remove dist

--- app/test/dist/page.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var bookmarks_json_1 = require("@/public/bookmarks.json");
-function TestPage() {
-    var bookmarks = bookmarks_json_1["default"];
-    return React.createElement("div", { className: "text-red-200" }, bookmarks[0].title);
-}
-exports["default"] = TestPage;
--- dist/prettier.config.dev.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")] // tabWidth: 4,
-
-};
