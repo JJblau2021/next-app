e0f8e2401fc37e4b0e4d6255acffafa1d2560852
shiyu <84095503+jjblau2021@users.noreply.github.com>
2023-06-27 14:46:50 +0800

    test: :white_check_mark: test

--- app/test/dist/page.js
+++ app/test/dist/page.js
@@ -3,6 +3,6 @@ exports.__esModule = true;
 var bookmarks_json_1 = require("@/public/bookmarks.json");
 function TestPage() {
     var bookmarks = bookmarks_json_1["default"];
-    return React.createElement("div", null, bookmarks[0].title);
+    return React.createElement("div", { className: "text-red-200" }, bookmarks[0].title);
 }
 exports["default"] = TestPage;
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,5 +3,5 @@ import json from "@/public/bookmarks.json";
 export default function TestPage() {
   const bookmarks = json;
 
-  return <div>{bookmarks[0].title}</div>;
+  return <div className="text-red-200">{bookmarks[0].title}</div>;
 }
