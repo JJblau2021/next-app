640489c62a573784a169458204e9cea8782ac678
jjblau <84095503+JJblau2021@users.noreply.github.com>
2023-07-03 22:48:28 +0800

    header

---
+++ app/Header.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+  window.addEventListener("scroll", (e) => {
+    const scrollTop = (e.target as Document).documentElement.scrollTop;
+    if (scrollTop > 64) {
+      document.getElementById("header")?.classList.add("bg-opacity-80");
+      document.getElementById("header")?.classList.add("shadow-md");
+    } else {
+      document.getElementById("header")?.classList.remove("bg-opacity-80");
+      document.getElementById("header")?.classList.remove("shadow-md");
+    }
+  });
+  return (
+    <header
+      id="header"
+      className="fixed top-0 flex h-16 w-full items-center bg-white bg-opacity-0  p-4 hover:bg-opacity-80 hover:shadow-md"
+    >
+      <div className="flex items-center">
+        <Image
+          width={32}
+          height={32}
+          alt="avatar"
+          src="https://avatars.githubusercontent.com/u/84095503?v=4"
+          className="mr-4 rounded-full"
+        />
+        <Link className="text-2xl font-bold uppercase" href="/">
+          jjblau2021
+        </Link>
+      </div>
+      <nav className="ml-auto flex gap-4">
+        <Link href="/">Home</Link>
+        <Link href="/bookmarks">Bookmarks</Link>
+      </nav>
+    </header>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
-"use client";
-import Image from "next/image";
+import Header from "./Header";
 import "./globals.css";
-import Link from "next/link";
 
 export const metadata = {
   title: "Create Next App",
@@ -13,16 +11,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  window.addEventListener("scroll", (e) => {
-    const scrollTop = (e.target as Document).documentElement.scrollTop;
-    if (scrollTop > 64) {
-      document.getElementById("header")?.classList.add("bg-opacity-80");
-      document.getElementById("header")?.classList.add("shadow-md");
-    } else {
-      document.getElementById("header")?.classList.remove("bg-opacity-80");
-      document.getElementById("header")?.classList.remove("shadow-md");
-    }
-  });
   return (
     <html lang="en">
       <head>
@@ -34,27 +22,7 @@ export default function RootLayout({
         />
       </head>
       <body>
-        <header
-          id="header"
-          className="fixed top-0 flex h-16 w-full items-center bg-white bg-opacity-0  p-4 hover:bg-opacity-80 hover:shadow-md"
-        >
-          <div className="flex items-center">
-            <Image
-              width={32}
-              height={32}
-              alt="avatar"
-              src="https://avatars.githubusercontent.com/u/84095503?v=4"
-              className="mr-4 rounded-full"
-            />
-            <Link className="text-2xl font-bold uppercase" href="/">
-              jjblau2021
-            </Link>
-          </div>
-          <nav className="ml-auto flex gap-4">
-            <Link href="/">Home</Link>
-            <Link href="/bookmarks">Bookmarks</Link>
-          </nav>
-        </header>
+        <Header />
         <main className="main p-20 pt-24">{children}</main>
       </body>
     </html>
