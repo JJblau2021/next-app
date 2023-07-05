daed97d73962287319bd99e0a6fd956c93a9ce3e
shiyu <84095503+jjblau2021@users.noreply.github.com>
2023-07-05 15:05:11 +0800

    feat: :sparkles: 修改 metadata

--- app/test/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <h5>layout</h5>
-      {children}
-    </div>
-  );
-}
--- app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-import json from "@/public/bookmarks.json";
-
-export default function TestPage() {
-  const bookmarks = json;
-
-  return <div className="text-red-200">{bookmarks[0].title}</div>;
-}
